c27d52ba9fc7422ec6d87af577952c8133401033
huanthemighty <huan.sun@gmail.com>
2016-07-17 23:46:07 -0700

    add symmetric tree

---
+++ SymmetricTree.py
@@ -0,0 +1,26 @@
+# Definition for a binary tree node.
+class TreeNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+class Solution(object):
+    def isSymmetric(self, root):
+        """
+        :type root: TreeNode
+        :rtype: bool
+        """
+        if root is None:
+            return True
+        else:
+            return self.isInverted(root.left, root.right)
+
+    def isInverted(self, p, q):
+        if p is None and q is None:
+            return True
+        if p is None or q is None:
+            return False
+        if p.val != q.val:
+            return False
+        return self.isInverted(p.left, q.right) and self.isInverted(p.right, q.left)
