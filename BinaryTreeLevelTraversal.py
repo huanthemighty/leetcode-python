4962900fb9517293fd4f1c36ad69c5442f345f4f
huanthemighty <huan.sun@gmail.com>
2016-07-23 22:03:27 -0700

    add binary tree level order traversal

---
+++ BinaryTreeLevelTraversal.py
@@ -0,0 +1,32 @@
+# Definition for a binary tree node.
+class TreeNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+class Solution(object):
+    def levelOrder(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[List[int]]
+        """
+        result = []
+        if root is None:
+            return result
+
+        current_level = [root]
+
+        while current_level:
+            next_level = []
+            current_val = []
+            for node in current_level:
+                current_val.append(node.val)
+                if node.left:
+                    next_level.append(node.left)
+                if node.right:
+                    next_level.append(node.right)
+            result.append(current_val)
+            current_level = next_level
+
+        return result
